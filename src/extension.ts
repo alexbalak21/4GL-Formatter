49df4976a3c5e3aac9b8d2657b4a8a1466698e06
alexbalak21 <alex.balak@outlook.com>
2025-02-04 16:48:20 +0100

    not working of formatting the code, cant select the ext

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,33 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.languages.registerDocumentFormattingEditProvider("x3", {
+      provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+        const edits: vscode.TextEdit[] = [];
+        let indentLevel = 0;
+        const indent = "  "; // Two spaces for indentation
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "4gl-formatter" is now active!');
+        for (let i = 0; i < document.lineCount; i++) {
+          const line = document.lineAt(i);
+          const trimmedLine = line.text.trim();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('4gl-formatter.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from 4GL-Formatter!');
-	});
+          if (trimmedLine.startsWith("Endif")) {
+            indentLevel--;
+          }
 
-	context.subscriptions.push(disposable);
+          const newText = indent.repeat(indentLevel) + trimmedLine;
+          if (newText !== line.text) {
+            edits.push(vscode.TextEdit.replace(line.range, newText));
+          }
+
+          if (trimmedLine.startsWith("If")) {
+            indentLevel++;
+          }
+        }
+
+        return edits;
+      },
+    })
+  );
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
